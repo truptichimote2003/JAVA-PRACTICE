18d3fe54ff83ade7d81b9768a3e0a2e04463d9ff
truptichimote2003 <129290401+truptichimote2003@users.noreply.github.com>
2023-03-29 18:24:25 +0530

    Add files via upload

---
+++ JointMethod.java
@@ -0,0 +1,25 @@
+//join() method
+class Mythread extends Thread{
+	public void run(){
+		for (int i=1;i<=10 ;++i ) {
+			System.out.println("child Thread");
+			try{
+				Thread.sleep(400);
+			}catch(InterruptedException ie){}
+		}
+	}
+}
+class JointMethod {
+	public static void main(String[] args) throws InterruptedException{
+		Mythread t=new Mythread();
+		t.start();
+		t.join();
+		for (int i=1;i<=10 ;++i ) {
+			System.out.println("Main Methiod");
+		}
+	}
+
+
+	}
+
+
